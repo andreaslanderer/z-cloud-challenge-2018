c187f97b50c8bfc4cd568565625155c591a6e2a3
Bruno Ritz (tzhribr4) <bruno.ritz@swisscom.com>
2018-11-19 14:32:44 +0100

    Convert to native SQS
    This allows using queue URLS

--- src/main/java/com/zuehlke/cloudchallenge/messagegransformer/message/FlightMessage.java
+++ src/main/java/com/zuehlke/cloudchallenge/messagegransformer/message/FlightMessage.java
@@ -1,12 +1,25 @@
 package com.zuehlke.cloudchallenge.messagegransformer.message;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class FlightMessage
 {
-	@JsonProperty("flight-number")
 	public String flightNumber;
 	public String airport;
 	public String message;
 	public String timestamp;
+
+	@JsonCreator
+	public FlightMessage(
+		@JsonProperty("flight-number") String flightNumber,
+		@JsonProperty("airport") String airport,
+		@JsonProperty("message") String message,
+		@JsonProperty("timestamp") String timestamp)
+	{
+		this.flightNumber = flightNumber;
+		this.airport = airport;
+		this.message = message;
+		this.timestamp = timestamp;
+	}
 }
---
+++ src/main/java/com/zuehlke/cloudchallenge/messagegransformer/sqs/SqsConfig.java
@@ -0,0 +1,80 @@
+package com.zuehlke.cloudchallenge.messagegransformer.sqs;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.messaging.config.QueueMessageHandlerFactory;
+import org.springframework.cloud.aws.messaging.config.SimpleMessageListenerContainerFactory;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.cloud.aws.messaging.listener.QueueMessageHandler;
+import org.springframework.cloud.aws.messaging.listener.SimpleMessageListenerContainer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+public class SqsConfig
+{
+	private final String awsAccessKey;
+	private final String awsSecretKey;
+
+	public SqsConfig(
+		@Value("${cloud.aws.credentials.accessKey}") String awsAccessKey,
+		@Value("${cloud.aws.credentials.secretKey}") String awsSecretKey)
+	{
+		this.awsAccessKey = awsAccessKey;
+		this.awsSecretKey = awsSecretKey;
+	}
+
+	@Bean
+	public QueueMessagingTemplate queueMessagingTemplate(AmazonSQSAsync amazonSqsAsync)
+	{
+		return new QueueMessagingTemplate(amazonSqsAsync);
+	}
+
+	@Bean(name = "amazonSqsAsync", destroyMethod = "shutdown")
+	public AmazonSQSAsync amazonSqsAsync()
+	{
+		return AmazonSQSAsyncClientBuilder.standard()
+			.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(awsAccessKey, awsSecretKey)))
+			.withRegion(Regions.EU_WEST_1)
+			.build();
+	}
+
+	@Bean
+	public SimpleMessageListenerContainer simpleMessageListenerContainer(QueueMessageHandler queueMessageHandler,
+		SimpleMessageListenerContainerFactory listenerContainerFactory)
+	{
+		SimpleMessageListenerContainer container = listenerContainerFactory.createSimpleMessageListenerContainer();
+
+		container.setMessageHandler(queueMessageHandler);
+
+		return container;
+	}
+
+	@Bean
+	public SimpleMessageListenerContainerFactory simpleMessageListenerContainerFactory(AmazonSQSAsync sqsAsync)
+	{
+		SimpleMessageListenerContainerFactory factor = new SimpleMessageListenerContainerFactory();
+
+		factor.setAmazonSqs(sqsAsync);
+
+		return factor;
+	}
+
+	@Bean
+	public QueueMessageHandler queueMessageHandler(AmazonSQSAsync sqsAsync)
+	{
+		QueueMessageHandlerFactory factory = new QueueMessageHandlerFactory();
+
+		factory.setAmazonSqs(sqsAsync);
+		factory.setMessageConverters(Collections.singletonList(new FlightMessageConverter()));
+
+		return factory.createQueueMessageHandler();
+	}
+}
