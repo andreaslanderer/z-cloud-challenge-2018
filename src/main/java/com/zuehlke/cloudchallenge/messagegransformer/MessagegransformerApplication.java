72efc0da4a003cac44aa41909c50590d43b6d434
Bruno Ritz (tzhribr4) <bruno.ritz@swisscom.com>
2018-11-19 09:16:07 +0100

    Initiali import

---
+++ src/main/java/com/zuehlke/cloudchallenge/messagegransformer/MessagegransformerApplication.java
@@ -0,0 +1,12 @@
+package com.zuehlke.cloudchallenge.messagegransformer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagegransformerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagegransformerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zuehlke/cloudchallenge/messagegransformer/MessagegransformerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zuehlke.cloudchallenge.messagegransformer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MessagegransformerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
