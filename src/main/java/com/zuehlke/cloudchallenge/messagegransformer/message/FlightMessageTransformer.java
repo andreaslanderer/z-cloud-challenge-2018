0c291687dc0135afff78fc21c5cef5922a3a6882
Bruno Ritz (tzhribr4) <bruno.ritz@swisscom.com>
2018-11-19 15:00:36 +0100

    Remove unused import

--- src/main/java/com/zuehlke/cloudchallenge/messagegransformer/message/FlightMessageTransformer.java
+++ src/main/java/com/zuehlke/cloudchallenge/messagegransformer/message/FlightMessageTransformer.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
-import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
